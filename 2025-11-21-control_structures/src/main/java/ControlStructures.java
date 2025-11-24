2d4df1f00f2c73efc779042e0a7fcf7c84d7c13b
Gunnar Hilling <gunnar@hilling.de>
2025-11-24 19:19:52 +0100

    Move samples program to correct module

--- 2025-11-21-control_structures/src/main/java/ControlStructures.java
+++ 2025-11-21-control_structures/src/main/java/ControlStructures.java
@@ -1,7 +1,13 @@
-/**
- * Demo for basic control structures in Java.
- */
 public class ControlStructures {
     public static void main(String[] args) {
+        System.out.println(args.toString());
+        int numArguments = args.length;
+        if(numArguments == 0) {
+            System.err.println("no args");
+        } else {
+            for(int currentArg=0; currentArg < numArguments; currentArg++) {
+                System.out.println(args[currentArg]);
+            }
+        }
     }
 }
