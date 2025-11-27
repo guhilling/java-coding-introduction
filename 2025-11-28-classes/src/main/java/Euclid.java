a39f1856be224469903f433ea712eb0168b08f98
Gunnar Hilling <gunnar@hilling.de>
2025-11-27 18:13:43 +0100

    - prepare next lecture - prepare demo projects

---
+++ 2025-11-28-classes/src/main/java/Euclid.java
@@ -0,0 +1,9 @@
+/**
+ * Euclidean algorithm.
+ * <br/>
+ * See especially the section about
+ * <a href="https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations">Implementations</a>
+ * on Wikipedia.
+ */
+public class Euclid {
+}
