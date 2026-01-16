7dc467f6a66ee9aab8f14f8b07739ea9cacdbca9
Gunnar Hilling <gunnar@hilling.de>
2026-01-16 12:20:59 +0100

    - Solution for tasks. - Skeleton for next lecture.

---
+++ 2026-01-16-CommonPropertiesOfAnObject/README.md
@@ -0,0 +1,25 @@
+# Plan for Common Object Properties
+
+## Class recap
+
+* Class
+* Interface
+* Abstract Class
+
+## What can we do with an _Object_?
+
+* Print it (`toString()`).
+* Compare it (`equals()`).
+  * Why do we need this?
+  * Let's take a look at the `Position` class.
+
+## Recap Exceptions
+
+* "Checked" vs. "Unchecked".
+* Stacktraces.
+
+## Introduce Collections
+
+* See `ChessGame` as an example.
+  * How to track the moves?
+  * Use a `List`!
---
+++ 2026-01-16-CommonPropertiesOfAnObject/src/main/java/de/hilling/chess/Position.java
@@ -0,0 +1,38 @@
+package de.hilling.chess;
+
+import java.util.regex.Pattern;
+
+/**
+ * Represents a position on the board.
+ * Objects of this class are immutable, correct coordinates are checked when the constructor is called.
+ */
+// TODO: Use static accessor to singletons
+public class Position {
+    private static final Pattern POSITION_PATTERN = Pattern.compile("[a-h][1-8]");
+
+    public final int x;
+    public final int y;
+    final char[] xCharacters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+    public Position(int x, int y) {
+        if(x<0 || y<0 || x>7 || y>7) {
+            throw new IllegalArgumentException("x and y need to be between 0 and 7");
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Position from(String positionString) {
+        if(POSITION_PATTERN.matcher(positionString).find()) {
+            return new Position(positionString.charAt(0) - 'a',
+                                Integer.parseInt(positionString.substring(1, 2)) - 1);
+        } else {
+            throw new IllegalArgumentException("Illegal position: " + positionString);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%c%d", xCharacters[x], y + 1);
+    }
+}
