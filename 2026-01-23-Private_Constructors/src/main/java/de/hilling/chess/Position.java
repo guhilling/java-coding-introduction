389bb689f5c4140e20f60faa88777e246afcd74d
Gunnar Hilling <gunnar@hilling.de>
2026-01-29 10:29:55 +0100

    - remove methods not needed any more.

--- 2026-01-23-Private_Constructors/src/main/java/de/hilling/chess/Position.java
+++ 2026-01-23-Private_Constructors/src/main/java/de/hilling/chess/Position.java
@@ -1,7 +1,5 @@
 package de.hilling.chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -12,23 +10,19 @@ import org.jspecify.annotations.NonNull;
  * Represents a position on the board.
  * Objects of this class are immutable, correct coordinates are checked when the constructor is called.
  */
-// TODO:
-//  * implement accessors.
-//  * collections of valid positions in static initializer.
-//  * then remove hashCode() and equals().
 
-public class Position extends Object{
-    private static final Pattern POSITION_PATTERN = Pattern.compile("[a-h][1-8]");
-    private static final char[] X_CHARACTERS = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-    private static final Map<String, Position> VALID_POSITIONS = new HashMap<>();
+public class Position extends Object {
+    private static final Pattern               POSITION_PATTERN = Pattern.compile("[a-h][1-8]");
+    private static final char[]                X_CHARACTERS     = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    private static final Map<String, Position> VALID_POSITIONS  = new HashMap<>();
 
-    public final int x;
-    public final int y;
+    public final int    x;
+    public final int    y;
     public final String formattedPosition;
 
     static {
-        for(int x = 0; x < 8; x++) {
-            for(int y = 0; y < 8; y++) {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
                 Position position = new Position(x, y);
                 VALID_POSITIONS.put(position.formattedPosition, position);
             }
@@ -39,17 +33,13 @@ public class Position extends Object{
         validatePosition(x, y);
         this.x = x;
         this.y = y;
-        formattedPosition = getFormattedPosition(x, y);
+        formattedPosition = String.format("%c%d", X_CHARACTERS[x], y + 1);
     }
 
     private static void validatePosition(int x, int y) {
-        if(x <0 || y <0 || x >7 || y >7) {
+        if (x < 0 || y < 0 || x > 7 || y > 7) {
             throw new IllegalArgumentException("x and y need to be between 0 and 7");
         }
-    }
-
-    private static @NonNull String getFormattedPosition(int x, int y) {
-        return String.format("%c%d", X_CHARACTERS[x], y + 1);
     }
 
     /**
@@ -57,12 +47,11 @@ public class Position extends Object{
      *
      * @param positionString position between "a1 and h8".
      * @return Position element
-     *
      * @throws IllegalArgumentException if positionString is invalid.
      */
     public static @NonNull Position of(@NonNull String positionString) {
         Position position = VALID_POSITIONS.get(positionString);
-        if(position == null) {
+        if (position == null) {
             throw new IllegalArgumentException("Illegal position: " + positionString);
         }
         return position;
@@ -74,34 +63,15 @@ public class Position extends Object{
      * @param x x-offset, starting at 0.
      * @param y y-offset, starting at 0
      * @return Position element
-     *
      * @throws IllegalArgumentException if x or y are not between 0 and 7.
      */
     public static @NonNull Position of(int x, int y) {
         validatePosition(x, y);
-        return of(getFormattedPosition(x, y));
+        return of(String.format("%c%d", X_CHARACTERS[x], y + 1));
     }
 
     @Override
     public String toString() {
         return formattedPosition;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (!(obj instanceof Position)) {
-            return false;
-        } else {
-            Position other = (Position) obj;
-            return x == other.x && y == other.y;
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return x + y * 8;
     }
 }
